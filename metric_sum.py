b785d570d426d95c789dd75c064bbff291d33ee5
TakeHome Incognito <take-home-incognito@eric-fischer.codes>
2021-07-21 16:47:13 -0400

    It works!
    It's functional, but only on the 10-second time period, and without much
    regard to "we're definitely going to run out of memory fast"

--- metric_sum.py
+++ metric_sum.py
@@ -1,22 +1,59 @@
+from datetime import datetime, timedelta
+
 from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+live_data = {}
+data_shape_str = """
+example after (correctly) posting 12345 to key "meow" twice:
+{
+    "meow":
+        [
+            [
+                (12345, <first_timestamp>),
+                (12345, <second_timestamp>)
+            ],
+            24690
+        ]
+}
+"""
+
+def _remove_old_data(key, timestamp):
+    for entry in live_data[key][0]:
+        if timestamp - entry[1] > timedelta(seconds=10):
+            live_data[key][1] -= entry[0]
+            live_data[key][0].remove(entry)
+        else:
+            break  # elements are in order!
+    if len(live_data[key][0]) == 0:
+        del live_data[key]
 
 @app.route("/metric/<key>/", methods=["POST"])
 def store_value(key):
     data = request.get_json()
     value = data.get('value', None)
     if value is not None:
-        print("recieved {}".format(value))
+        if key not in live_data.keys():
+            live_data[key] = [[], 0]
+        now = datetime.now()
+        live_data[key][0].append((value, now))
+        live_data[key][1] += value
+        _remove_old_data(key, now)
         return {}, 200
-    else:
+    else:  # TODO: handle errors/validation with proper HTTP codes
         return {}, 500
 
 
 @app.route("/metric/<key>/sum", methods=["GET"])
 def read_value(key):
-    return {"value": 0}, 200
+    try:
+        _remove_old_data(key, datetime.now())
+    except KeyError:
+        pass  # key wasn't previously recorded, we'll return 0 below
+
+    ret_value = live_data.get(
+        key,
+        ([], 0)
+    )[1]
+    return {"value": ret_value}, 200
