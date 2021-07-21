949ef757a28da49ffb0ba560678fc827375b97e3
TakeHome Incognito <take-home-incognito@eric-fischer.codes>
2021-07-21 15:54:01 -0400

    Ensure incoming requests are read
    tested via the following:
    `curl -H "Content-Type: application/json" --data '{"value":12345}'   http://localhost:5000/metric/meow/`

--- metric_sum.py
+++ metric_sum.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+@app.route("/metric/<key>/", methods=["POST"])
+def store_value(key):
+    data = request.get_json()
+    value = data.get('value', None)
+    if value is not None:
+        print("recieved {}".format(value))
+        return {}, 200
+    else:
+        return {}, 500
